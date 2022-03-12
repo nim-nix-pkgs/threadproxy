c0a4acf67a671ea12ecdfccdb57a88125ac81698
hugosenari <hugosenari@gmail.com>
2022-03-12 03:34:40 -0300

    chore: re index threadproxy

---
+++ 0_0_2/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''Simplify Nim Inter-Thread Communication'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-threadproxy-0_0_2.flake = false;
+  inputs.src-threadproxy-0_0_2.owner = "jackhftang";
+  inputs.src-threadproxy-0_0_2.ref   = "refs/tags/0.0.2";
+  inputs.src-threadproxy-0_0_2.repo  = "threadproxy.nim";
+  inputs.src-threadproxy-0_0_2.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-threadproxy-0_0_2"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-threadproxy-0_0_2";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
---
+++ 0_2_1/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''Simplify Nim Inter-Thread Communication'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-threadproxy-0_2_1.flake = false;
+  inputs.src-threadproxy-0_2_1.owner = "jackhftang";
+  inputs.src-threadproxy-0_2_1.ref   = "refs/tags/0.2.1";
+  inputs.src-threadproxy-0_2_1.repo  = "threadproxy.nim";
+  inputs.src-threadproxy-0_2_1.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-threadproxy-0_2_1"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-threadproxy-0_2_1";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
