b4c4649b2c92c2189e983957e699251a79f73c82
hugosenari <hugosenari@gmail.com>
2022-03-22 01:17:57 -0300

    chore: re-index ../nimpkgs/t/threadproxy

--- 0_0_1/flake.nix
+++ 0_0_1/flake.nix
@@ -8,8 +8,8 @@
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
   inputs.src-threadproxy-0_0_1.flake = false;
+  inputs.src-threadproxy-0_0_1.ref   = "refs/tags/0.0.1";
   inputs.src-threadproxy-0_0_1.owner = "jackhftang";
-  inputs.src-threadproxy-0_0_1.ref   = "0_0_1";
   inputs.src-threadproxy-0_0_1.repo  = "threadproxy.nim";
   inputs.src-threadproxy-0_0_1.type  = "github";
   
